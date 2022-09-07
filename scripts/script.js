14d2f9a48620df1df81c81d5e8991785de1c5e75
Fahad Akram <fadiii3337@gmail.com>
2022-09-07 19:54:48 +0500

    added drag and drop feature for desktop and did some code optimizations and cleaning

--- scripts/script.js
+++ scripts/script.js
@@ -13,8 +13,6 @@ const viewActiveMobile = document.querySelector('.view-active-mobile');
 const viewCompletedMobile = document.querySelector('.view-completed-mobile');
 const clearCompletedTasks = document.querySelector('.clear-completed-tasks');
 
-let numberOfTasks = 0;
-
 function changeTheme() {
     if ((themeIcon.src).includes('moon')) {
         themeIcon.src = 'images/icon-sun.svg';
@@ -34,22 +32,28 @@ function changeTheme() {
 
 themeIcon.addEventListener('click', changeTheme);
 
+let taskId = 0;
+let numberOfTasksLeft = 0;
+
 function addTaskToList() {
     const task = document.createElement('div');
     task.setAttribute('class', 'task');
+    task.setAttribute('draggable', 'true');
+    taskId++;
+    task.setAttribute('id', `task${taskId}`);
     task.innerHTML =
-    `<label class="checkbox-container">
-        <input type="checkbox" class="entered-task-check">
-        <span class="checkmark"></span>
+    `<label class='checkbox-container'>
+        <input type='checkbox' class='entered-task-check'>
+        <span class='checkmark'></span>
     </label>
-    <p class="entered-task-text">${inputField.value}</p>
-    <img class="x-icon" src="images/icon-cross.svg">`;
+    <p class='entered-task-text'>${inputField.value}</p>
+    <img class='x-icon' src='images/icon-cross.svg' draggable='false'>`;
     taskList.appendChild(task);
-    numberOfTasks++;
+    numberOfTasksLeft++;
 }
 
-function updateNumberOfTasks() {
-    itemsLeft.textContent = `${numberOfTasks} items left`;
+function updateNumberOfTasksLeft() {
+    itemsLeft.textContent = `${numberOfTasksLeft} items left`;
 }
 
 function toggleNoTasksMsg() {
@@ -61,57 +65,60 @@ function toggleNoTasksMsg() {
     }
 }
 
-inputField.addEventListener('keydown', function (e) {
+function callFunctionsForInputField(e){
     if (e.key === 'Enter' && inputField.value != '') {
         addTaskToList();
-        updateNumberOfTasks();
+        updateNumberOfTasksLeft();
         toggleNoTasksMsg();
         inputField.value = '';
     }
-});
+}
+
+inputField.addEventListener('keydown', callFunctionsForInputField);
+
+function removeTask(e) {
+    const taskDivChildNodes = e.target.parentNode.childNodes;
+    e.target.parentNode.remove();
+    if (!taskDivChildNodes[0].childNodes[1].checked) {
+        numberOfTasksLeft--;
+    }
+    updateNumberOfTasksLeft();
+    toggleNoTasksMsg();
+}
+
+function taskChecked(e) {
+    const taskDivChildNodes = e.target.parentNode.parentNode.childNodes;
+    taskDivChildNodes.forEach(child => {
+        if (child.tagName === 'P') {
+            child.classList.toggle('checked-task');
+        }
+    });
+    if (e.target.checked) {
+        numberOfTasksLeft--;
+        updateNumberOfTasksLeft();
+    }
+    else {
+        numberOfTasksLeft++;
+        updateNumberOfTasksLeft();
+    }
+}
 
 taskList.addEventListener('click', function (e) {
-    let childNodes = undefined;
     if (e.target.className === 'x-icon') {
-        childNodes = e.target.parentNode.childNodes;
-        e.target.parentNode.remove();
-        let label = undefined;
-        childNodes.forEach(child => {
-            if (child.className === 'checkbox-container') {
-                label = child;
-            }
-        });
-        if (!label.childNodes[1].checked) {
-            numberOfTasks--;
-        }
-        updateNumberOfTasks();
-        toggleNoTasksMsg();
+        removeTask(e);
     }
     if (e.target.className === 'entered-task-check') {
-        childNodes = e.target.parentNode.parentNode.childNodes;
-        childNodes.forEach(child => {
-            if (child.tagName === 'P') {
-                child.classList.toggle('checked-task');
-            }
-        });
-        if (e.target.checked) {
-            numberOfTasks--;
-            updateNumberOfTasks();
-        }
-        else {
-            numberOfTasks++;
-            updateNumberOfTasks();
-        }
+        taskChecked(e);
     }
 });
 
-function changeViewOptionCSS(op1, op2, op3) {
-    op1.classList.add('active-view-option');
-    op1.classList.remove('non-active-view-option');
-    op2.classList.add('non-active-view-option');
-    op2.classList.remove('active-view-option');
-    op3.classList.add('non-active-view-option');
-    op3.classList.remove('active-view-option');
+function changeCssOfViewOption(activeBtn, UnactiveBtn1, UnactiveBtn2) {
+    activeBtn.classList.add('active-view-option');
+    activeBtn.classList.remove('non-active-view-option');
+    UnactiveBtn1.classList.add('non-active-view-option');
+    UnactiveBtn1.classList.remove('active-view-option');
+    UnactiveBtn2.classList.add('non-active-view-option');
+    UnactiveBtn2.classList.remove('active-view-option');
 }
 
 function showAllTasks() {
@@ -151,50 +158,44 @@ function showCompletedTasks() {
     });
 }
 
-viewAllDesktop.addEventListener('click', function (e) {
-    let target = e.target;
-    changeViewOptionCSS(target, viewActiveDesktop, viewCompletedDesktop);
+viewAllDesktop.addEventListener('click', function(e){
+    changeCssOfViewOption(e.target, viewActiveDesktop, viewCompletedDesktop);
     showAllTasks();
     toggleNoTasksMsg();
 });
 
-viewActiveDesktop.addEventListener('click', function (e) {
-    let target = e.target;
-    changeViewOptionCSS(target, viewAllDesktop, viewCompletedDesktop);
+viewActiveDesktop.addEventListener('click', function(e){
+    changeCssOfViewOption(e.target, viewAllDesktop, viewCompletedDesktop);
     showActiveTasks();
     toggleNoTasksMsg();
 });
 
-viewCompletedDesktop.addEventListener('click', function (e) {
-    let target = e.target;
-    changeViewOptionCSS(target, viewAllDesktop, viewActiveDesktop);
+viewCompletedDesktop.addEventListener('click', function(e){
+    changeCssOfViewOption(e.target, viewAllDesktop, viewActiveDesktop);
     showCompletedTasks();
     toggleNoTasksMsg();
 });
 
-viewAllMobile.addEventListener('click', function (e) {
-    let target = e.target;
-    changeViewOptionCSS(target, viewActiveMobile, viewCompletedMobile);
+viewAllMobile.addEventListener('click', function(e){
+    changeCssOfViewOption(e.target, viewActiveMobile, viewCompletedMobile);
     showAllTasks();
     toggleNoTasksMsg();
 });
 
-viewActiveMobile.addEventListener('click', function (e) {
-    let target = e.target;
-    changeViewOptionCSS(target, viewAllMobile, viewCompletedMobile);
+viewActiveMobile.addEventListener('click', function(e){
+    changeCssOfViewOption(e.target, viewAllMobile, viewCompletedMobile);
     showActiveTasks();
     toggleNoTasksMsg();
 });
 
-viewCompletedMobile.addEventListener('click', function (e) {
-    let target = e.target;
-    changeViewOptionCSS(target, viewAllMobile, viewActiveMobile);
+viewCompletedMobile.addEventListener('click', function(e){
+    changeCssOfViewOption(e.target, viewAllMobile, viewActiveDesktop);
     showCompletedTasks();
     toggleNoTasksMsg();
 });
 
-window.onload = changeViewOptionCSS(viewAllDesktop, viewActiveDesktop, viewCompletedDesktop);
-window.onload = changeViewOptionCSS(viewAllMobile, viewActiveMobile, viewCompletedMobile);
+window.onload = changeCssOfViewOption(viewAllDesktop, viewActiveDesktop, viewCompletedDesktop);
+window.onload = changeCssOfViewOption(viewAllMobile, viewActiveMobile, viewCompletedMobile);
 
 function clearCompleted() {
     const tasks = taskList.childNodes;
@@ -209,3 +210,58 @@ function clearCompleted() {
 }
 
 clearCompletedTasks.addEventListener('click', clearCompleted);
+
+function dragStart(e) {
+    if (e.target.className === 'task') {
+        e.dataTransfer.setData('text', e.target.id);
+    }
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    if (e.target.className === 'task') {
+        e.target.classList.add('drag-over');
+    }
+}
+
+function dragOver(e) {
+    e.preventDefault();
+    if (e.target.className === 'task') {
+        e.target.classList.add('drag-over')
+    }
+}
+
+function dragLeave(e) {
+    e.target.classList.remove('drag-over');
+}
+
+let dragIndex = 0;
+let clone = ' ';
+
+function drop(e) {
+    clone = e.target.cloneNode(true);
+    let data = e.dataTransfer.getData('text');
+    if (clone.id !== data && e.target.className === 'task drag-over') {
+        let nodelist = taskList.childNodes;
+        for (let i = 1; i < nodelist.length; i++) {
+            if (nodelist[i].id === data) {
+                dragIndex = i;
+            }
+        }
+        taskList.replaceChild(document.getElementById(data), e.target);
+        taskList.insertBefore(clone, taskList.childNodes[dragIndex]);
+        clone.classList.remove('drag-over');
+    }
+    e.target.classList.remove('drag-over');
+}
+
+taskList.addEventListener('mousedown', function () {
+    let tasks = taskList.childNodes;
+    tasks.forEach(task => {
+        task.addEventListener('dragstart', dragStart);
+        task.addEventListener('dragenter', dragEnter);
+        task.addEventListener('dragover', dragOver);
+        task.addEventListener('dragleave', dragLeave);
+        task.addEventListener('drop', drop);
+    });
+});
